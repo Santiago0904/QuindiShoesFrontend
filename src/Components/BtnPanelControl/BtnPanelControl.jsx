afdd724fc6ae8734b3e9b09209dc65cf50b6b972
Micahel Conde <michaelconde387@gmail.com>
2025-05-26 12:18:10 -0500

    Actualizacion de los botones del backoffice

--- src/Components/BtnPanelControl/BtnPanelControl.jsx
+++ src/Components/BtnPanelControl/BtnPanelControl.jsx
@@ -5,11 +5,11 @@ export const BtnPanelControl = ({ icon, title, bgColor, onClick }) => {
   return (
     <motion.div
       whileHover={{ scale: 1.05 }}
-      className={`flex flex-col items-center justify-center rounded-3xl p-5 h-[380px] w-[360px] cursor-pointer transition-all shadow-md ${bgColor}`}
+      className={`flex flex-col items-center justify-center rounded-3xl p-5 h-[480px] w-[360px] cursor-pointer transition-all shadow-md ${bgColor}`}
       onClick={onClick}
     >
       <div className="mb-2">
-        <img src={icon} alt={title} className="w-70 h-50" />
+        <img src={icon} alt={title} className="w-70 h-70" />
       </div>
       <h3 className="font-semibold text-neutral-800 text-[30px]">{title}</h3>
     </motion.div>
