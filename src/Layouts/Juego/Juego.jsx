c4f45241fca6ac03c20d74a110a4702c4560ee4a
CamiloDuranMesa <duranmesacamilo@gmail.com>
2025-07-07 14:14:48 -0500

    Correción alert descuento

--- src/Layouts/Juego/Juego.jsx
+++ src/Layouts/Juego/Juego.jsx
@@ -275,13 +275,10 @@ const gameLoop = () => {
       setScore(prev => {
         const newScore = prev + 1;
         scoreRef.current = newScore;
-        // Eliminado el sonido aquí:
-        // if (newScore % 10 === 0) {
-        //   const point = pointSound.current;
-        //   point.pause();
-        //   point.currentTime = 0;
-        //   point.play().catch((e) => console.warn("No se pudo reproducir point.mp3:", e));
-        // }
+        
+        if([5, 10, 15].includes(newScore)) {
+          checkAndSetDescuento(newScore);
+        }
         return newScore;
       });
       pipe.scored = true;
