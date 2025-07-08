8600d719a92675bf532bf7236438427e706dcf46
santiagoAguirreCastano <santiagoaguirrecastano8@gmail.com>
2025-07-07 19:45:34 -0500

    Merge branch 'develop' into Santiagobustos

--- src/Pages/Carrito/Carrito.jsx
+++ src/Pages/Carrito/Carrito.jsx
@@ -105,7 +105,7 @@ const Carrito = () => {
       country: "co",
       method: "POST",
       response: "https://quindi-shoes-frontend-yemj.vercel.app/",
-      confirmation: "https://quindishoes-backend-def.onrender.com/api/pagos/confirmacion",
+      confirmation: "http://localhost:5173/api/pagos/confirmacion",
       external: "false",
       x_extra1: userId.toString(),
       x_extra2: JSON.stringify(carritoReducido),
--- src/api/axion.js
+++ src/api/axion.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosClient = axios.create({
-  baseURL: "https://quindishoes-backend-def.onrender.com",
+  baseURL: "http://localhost:5173",
   headers: {
     "Content-Type": "application/json",
   },
